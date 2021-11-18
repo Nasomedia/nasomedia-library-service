4bfd4247e470aedf35d8fa12e82c7f52a1d8230d
hwc9169 <hwc9169@gmail.com>
2021-11-18 16:05:16 +0900

    get_series 메서드 추가

--- app/deps/main_service.py
+++ app/deps/main_service.py
@@ -7,11 +7,21 @@ import aiohttp
 
 
 class MainService():
-    @classmethod
-    async def get_episode(cls, episode_id: int) -> schemas.Episode:
+    async def get_episode(self, episode_id: int) -> schemas.Episode:
         async with aiohttp.ClientSession() as session:
             async with session.get(f"{settings.MAIN_SERVICE_BASE_URL}/api/v1/episodes/{episode_id}") as resp:
                 if resp.status != 200:
                     raise HTTPException(status_code=resp.status, detail=await resp.text())
                 episode_data = await resp.json()
                 return schemas.Episode(**episode_data)
+
+    
+    async def get_series(self, series_id: int) -> schemas.Series:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{settings.MAIN_SERVICE_BASE_URL}/api/v1/serieses/{series_id}") as resp:
+                if resp.status != 200:
+                    raise HTTPException(status_code=resp.status, detail=await resp.text())
+                episode_data = await resp.json()
+                return schemas.Series(**episode_data)
+
+    
