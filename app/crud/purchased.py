d1b0d48f95e58946b00bd166c8e4fb776e7b522e
hwc9169 <hwc9169@gmail.com>
2021-11-18 16:04:49 +0900

    crud 기능 추가

--- app/crud/purchased.py
+++ app/crud/purchased.py
@@ -7,6 +7,35 @@ from app.models import Purchased
 from app.schemas import PurchasedCreate, PurchasedUpdate
 
 class CRUDPurchased(CRUDBase[Purchased, PurchasedCreate, PurchasedUpdate]):
-    pass
+    def get_multi_with_user(
+        self, db: Session, *, user_id: int
+    ) -> List[Purchased]:
+        return db.query(self.model)\
+            .filter(self.model.user_id == user_id)\
+            .all()
+
+    def get_with_user_and_episode(
+        self, 
+        db: Session, 
+        *, 
+        user_id:int, 
+        episode_id: int
+    ) -> Purchased:
+        return db.query(self.model)\
+            .filter(self.model.user_id == user_id)\
+            .filter(self.model.episode_id == episode_id)\
+            .first()
+
+    def create_with_user_episode(
+        self, db: Session, *, user_id: int, episode_id: int
+    ) -> Purchased:
+        db_obj = self.model(
+            user_id=user_id, 
+            episode_id=episode_id,
+        )
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
 
 purchased = CRUDPurchased(Purchased)
